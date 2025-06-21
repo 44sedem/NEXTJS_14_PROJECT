ef94a12954a8c28b3950ba241ff2386758bbc7c8
Fiavor Isaac Sedem <fiavorisaac46@gmail.com>
2025-06-21 15:04:22 +0000

    Change of Database

--- Computer_Engineering_Department_Database.sql
+++ Computer_Engineering_Department_Database.sql
@@ -37,13 +37,13 @@
 -- 	FOREIGN KEY (student_number) REFERENCES cped.students(student_number),
 -- 	FOREIGN KEY (course_code) REFERENCES cped.courses(course_code)
 
-
 -- );
 
 -- Lecturers table
 -- CREATE TABLE cped.lecturers (
 --     lecturer_name VARCHAR(100) PRIMARY KEY,
---     email VARCHAR(100)
+--     email VARCHAR(100),
+-- 	Phone_Number VARCHAR(30)
 -- );
 
 -- TAs table
@@ -51,17 +51,25 @@
 --  	TAs_name VARCHAR(100) PRIMARY KEY,
 -- 		lecturer_name VARCHAR(100),
 --   	email VARCHAR(100),
+-- 	  TAs_Phone_Number VARCHAR(30),
 -- 		FOREIGN KEY(lecturer_name) REFERENCES cped.lecturers(lecturer_name)
 -- );
+
+-- ALTER TABLE cped.courses
+-- ADD CONSTRAINT unique_course_name UNIQUE(course_name);
 
 -- Lecturer to Course Assignment table
 -- CREATE TABLE cped.lecturer_courses (
 --     id SERIAL PRIMARY KEY,
 -- 	lecturer_name VARCHAR(100),
 -- 	course_code VARCHAR(20),
+-- 	course_name VARCHAR(100),
 --     FOREIGN KEY(lecturer_name) REFERENCES cped.lecturers(lecturer_name),
---     FOREIGN KEY(course_code) REFERENCES cped.courses(course_code)
+--     FOREIGN KEY(course_code) REFERENCES cped.courses(course_code),
+-- 	FOREIGN KEY(course_name) REFERENCES cped.courses(course_name)
+
 -- );
+
 
 -- Lecturer to TA Assignment table
 -- CREATE TABLE cped.lecturer_TAs (
@@ -114,7 +122,7 @@
 -- ('Nuku-Tagbor Joshua', '11252857', 'M', 'joshuatagbor@gmail.com'),
 -- ('Desmond Afelete Kamasah', '11253931', 'M', 'kamasahdesmond@gmail.com'),
 -- ('Fordjour Edward John', '11254079', 'M', 'johnfordjour@gmail.com'),
--- ('Kudiabor Jonathan Kwabena Ewenam', 'M', '11254301', 'ewenamjonathan@gmail.com'),
+-- ('Kudiabor Jonathan Kwabena Ewenam', '11254301', 'M', 'ewenamjonathan@gmail.com'),
 -- ('Abena Nhyira Nsaako', '11254405', 'F', 'nsaakoabena@gmail.com'),
 -- ('Dedoo Donatus Dodzi', '11262592', 'M', 'dodzidonatus@gmail.com'),
 -- ('Ayertey Vanessa Esinam', '11264010', 'F', 'esinamneesa@gmail.com'),
@@ -133,60 +141,49 @@
 -- ('Ahmed Fareed Opare', '11338323', 'M', 'ahmedfareed@gmail.com'),
 -- ('Derrick Amponsah Amponsah', '11347946', 'M', 'amponsahderrick@gmail.com'),
 -- ('Freda Elikplim Apetsi', '11348310', 'F', 'fredaelikplim@gmail.com'),
--- ('Dabanka Hayet Maame Adwoa Gyasiwaa', 'F', '11353826', 'dabankahayet@gmail.com'),
+-- ('Dabanka Hayet Maame Adwoa Gyasiwaa','11353826', 'F', 'dabankahayet@gmail.com'),
 -- ('Edward Opoku Agyemang', '11356825', 'M', 'agyemangedward@gmail.com'),
 -- ('Nana Kwasi Kwakye', '11358243', 'F', 'nanakwakye@gmail.com');
 
 -- SELECT * FROM cped.students
 
--- UPDATE cped.students
--- SET student_number = '11254301', gender = 'M'
--- WHERE student_id = 40;
-
--- UPDATE cped.students
--- SET student_number = '11353826', gender = 'F' 
--- WHERE student_id = 59;
-
--- ALTER TABLE cped.fees_payments
---  DROP COLUMN status;
-
 -- INSERT INTO cped.fees_payments (student_number, amount_paid, total_fee, payment_date) VALUES
--- ('10975105', 500.00, 2992.00, '2024-01-10'),
+-- ('10975105', 2992.00, 2992.00, '2024-01-10'),
 -- ('11004272', 300.00,  2992.00, '2024-02-15'),
 -- ('11010910', 200.00,  2992.00, '2024-03-20'),
--- ('11012330', 400.00,  2992.00, '2024-04-10'),
+-- ('11012330', 2992.00,  2992.00, '2024-04-10'),
 -- ('11012343', 600.00,  2992.00, '2024-05-15'),
 -- ('11014727', 450.00,  2992.00, '2024-01-20'),
 -- ('11014977', 350.00,  2992.00, '2024-02-25'),
--- ('11015506', 500.00,  2992.00, '2024-03-30'),
+-- ('11015506', 2992.00,  2992.00, '2024-03-30'),
 -- ('11018690', 250.00,  2992.00, '2024-04-05'),
 -- ('11021544', 300.00,  2992.00, '2024-05-10'),
--- ('11023595', 700.00,  2992.00, '2024-01-15'),
+-- ('11023595', 2992.00,  2992.00, '2024-01-15'),
 -- ('11025159', 200.00,  2992.00, '2024-02-20'),
 -- ('11038081', 400.00,  2992.00, '2024-03-25'),
 -- ('11049492', 600.00,  2992.00, '2024-04-30'),
 -- ('11049523', 500.00, 2992.00,  '2024-05-05'),
 -- ('11053386', 300.00, 2992.00, '2024-01-18'),
 -- ('11105235', 200.00, 2992.00, '2024-03-28'),
--- ('11112276', 400.00, 2992.00, '2024-03-28'),
+-- ('11112276', 2992.00, 2992.00, '2024-03-28'),
 -- ('11116537', 600.00,  2992.00, '2024-04-02'),
 -- ('11116737', 500.00,  2992.00, '2024-05-07'),
 -- ('11116804', 300.00,  2992.00, '2024-01-20'),
 -- ('11117318', 200.00,  2992.00, '2024-02-25'),
 -- ('11117536', 400.00,  2992.00, '2024-03-30'),
 -- ('11123762', 600.00,  2992.00, '2024-04-05'),
--- ('11139245', 500.00, 2992.00,  '2024-05-10'),
+-- ('11139245', 2992.00, 2992.00,  '2024-05-10'),
 -- ('11139828', 300.00,  2992.00, '2024-01-22'),
 -- ('11164744', 200.00, 2992.00,  '2024-02-27'),
 -- ('11170350', 400.00,  2992.00, '2024-03-05'),
 -- ('11172376', 600.00,  2992.00, '2024-04-10'),
--- ('11208328', 500.00,  2992.00, '2024-05-15'),
+-- ('11208328', 2992.00,  2992.00, '2024-05-15'),
 -- ('11209640', 300.00,  2992.00, '2024-01-25'),
 -- ('11213307', 200.00,  2992.00, '2024-02-28'),
 -- ('11218951', 400.00,  2992.00, '2024-03-10'),
 -- ('11238291', 600.00, 2992.00,  '2024-04-15'),
 -- ('11246366', 500.00,  2992.00, '2024-05-20'),
--- ('11252855', 300.00,  2992.00, '2024-01-28'),
+-- ('11252855', 2992.00,  2992.00, '2024-01-28'),
 -- ('11252857', 200.00,  2992.00, '2024-02-28'),
 -- ('11253931', 400.00, 2992.00,  '2024-03-15'),
 -- ('11254079', 600.00,  2992.00, '2024-04-20'),
@@ -198,7 +195,7 @@
 -- ('11285635', 500.00,  2992.00, '2024-05-27'),
 -- ('11292620', 300.00,  2992.00, '2024-02-02'),
 -- ('11293871', 200.00,  2992.00, '2024-03-04'),
--- ('11296641', 400.00,  2992.00, '2024-03-19'),
+-- ('11296641', 2992.00,  2992.00, '2024-03-19'),
 -- ('11296662', 600.00,  2992.00, '2024-04-24'),
 -- ('11297849', 500.00,  2992.00, '2024-05-29'),
 -- ('11305528', 300.00,  2992.00, '2024-02-04'),
@@ -211,7 +208,8 @@
 -- ('11348310', 400.00,  2992.00, '2024-03-23'),
 -- ('11353826', 600.00,  2992.00, '2024-04-28'),
 -- ('11356825', 750.00,  2992.00, '2024-03-20'),
--- ('11356825', 750.00,  2992.00, '2024-03-20');
+-- ('11356825', 2992.00,  2992.00, '2024-03-20');
+
 
 -- SELECT * FROM cped.fees_payments
 
@@ -312,28 +310,51 @@
 -- ('SENG202', 'Dr. John Kutor'),
 -- ('CBAS210', 'Dr. Percy Okae');
 
+-- INSERT INTO cped.lecturer_courses (course_name) VALUES
+-- ('Computer system design'),
+-- ('Data structures and Algorithms'),
+-- ('Linear circuits'),
+-- ('Software Engineering'),
+-- ('Data Communications'),
+-- ('Differential equations'),
+-- ('Academic Writing II');
+
+-- DELETE FROM cped.lecturer_courses
+-- WHERE course_code IS NULL AND lecturer_name IS NULL;
+
 -- SELECT * FROM cped.lecturer_courses
+
+-- UPDATE cped.lecturer_courses
+-- SET course_name = CASE course_code
+--     WHEN 'CPEN 202' THEN 'Computer system design'
+--     WHEN 'CPEN 204' THEN 'Data structures and Algorithms'
+--     WHEN 'CPEN 206' THEN 'Linear circuits'
+--     WHEN 'CPEN 208' THEN 'Software Engineering'
+--     WHEN 'CPEN 212' THEN 'Data Communications'
+--     WHEN 'SENG202' THEN 'Differential equations'
+--     WHEN 'CBAS210' THEN 'Academic Writing II'
+--     ELSE course_name  -- Leave unchanged if no match
+-- END
+-- WHERE course_name IS NULL;
+
 
 -- INSERT INTO cped.TAs (TAs_name) VALUES
 -- ('Bamzy'),
--- ('Foster'),
--- ('Hakeem'),
--- ('Samed'),
+-- ('Kudus'),
+-- ('Prince'),
+-- ('Akua'),
 -- ('Ben'),
--- ('Kevin');
-
+-- ('Kevin'),
+-- ('Dave');
 
 -- INSERT INTO cped.lecturer_TAs (lecturer_name, TAs_name) VALUES
 -- ('Agyare Debrah', 'Bamzy'),
--- ('Dr. Margaret Ansah Richardson', 'Foster'),
--- ('Dr. Godfrey Augustus Mills', 'Hakeem'),
--- ('Mr. John Asiammah', 'Foster'),
--- ('Agyare Debrah', 'Samed'),
+-- ('Dr. Margaret Ansah Richardson', 'Kudus'),
+-- ('Dr. Godfrey Augustus Mills', 'Prince'),
+-- ('Mr. John Asiammah', 'Akua'),
+-- ('Dr. Percy Okae', 'Dave'),
 -- ('Dr. John Kutor', 'Ben'),
 -- ('Dr. Isaac Adjaye Aboagye', 'Kevin');
-
-
-
 
 -- ALTER TABLE cped.fees_payments ADD COLUMN status TEXT;
 
@@ -362,6 +383,125 @@
 --     RETURN NEW;
 -- END;
 -- $$ LANGUAGE plpgsql;
+
+-- DROP FUNCTION IF EXISTS cped.set_payment_status();
+
+-- CREATE OR REPLACE FUNCTION cped.update_payment_status()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--     IF NEW.amount_paid >= NEW.total_fee THEN
+--         NEW.status := 'Paid';
+--     ELSE
+--         NEW.status := 'Owing';
+--     END IF;
+--     RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- CREATE TRIGGER trg_update_payment_status
+-- BEFORE INSERT OR UPDATE ON cped.fees_payments
+-- FOR EACH ROW
+-- EXECUTE FUNCTION cped.update_payment_status();
+
+
+-- SELECT * FROM cped.fees_payments
+
+
+-- SELECT tgname, tgrelid::regclass AS table_name
+-- FROM pg_trigger
+-- WHERE tgname = 'trg_update_payment_status';
+
+-- DROP TRIGGER IF EXISTS trg_update_payment_status ON cped.fees_payments;
+-- DROP FUNCTION IF EXISTS cped.update_payment_status();
+
+-- DELETE FROM cped.fees_payments;
+
+-- CREATE OR REPLACE FUNCTION cped.update_payment_status()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--     IF NEW.amount_paid IS NOT NULL AND NEW.total_fee IS NOT NULL THEN
+--         IF NEW.amount_paid >= NEW.total_fee THEN
+--             NEW.status := 'Paid';
+--         ELSE
+--             NEW.status := 'Owing';
+--         END IF;
+--     END IF;
+--     RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+-- DROP TRIGGER IF EXISTS trg_update_payment_status ON cped.fees_payments;
+
+-- CREATE TRIGGER trg_update_payment_status
+-- BEFORE INSERT OR UPDATE ON cped.fees_payments
+-- FOR EACH ROW
+-- EXECUTE FUNCTION cped.update_payment_status();
+
+-- INSERT INTO cped.fees_payments (student_number, amount_paid, total_fee, payment_date) VALUES
+-- ('10975105', 2992.00, 2992.00, '2024-01-10'),
+-- ('11004272', 300.00,  2992.00, '2024-02-15'),
+-- ('11010910', 200.00,  2992.00, '2024-03-20'),
+-- ('11012330', 2992.00,  2992.00, '2024-04-10'),
+-- ('11012343', 600.00,  2992.00, '2024-05-15'),
+-- ('11014727', 450.00,  2992.00, '2024-01-20'),
+-- ('11014977', 350.00,  2992.00, '2024-02-25'),
+-- ('11015506', 2992.00,  2992.00, '2024-03-30'),
+-- ('11018690', 250.00,  2992.00, '2024-04-05'),
+-- ('11021544', 300.00,  2992.00, '2024-05-10'),
+-- ('11023595', 2992.00,  2992.00, '2024-01-15'),
+-- ('11025159', 200.00,  2992.00, '2024-02-20'),
+-- ('11038081', 400.00,  2992.00, '2024-03-25'),
+-- ('11049492', 600.00,  2992.00, '2024-04-30'),
+-- ('11049523', 500.00, 2992.00,  '2024-05-05'),
+-- ('11053386', 300.00, 2992.00, '2024-01-18'),
+-- ('11105235', 200.00, 2992.00, '2024-03-28'),
+-- ('11112276', 2992.00, 2992.00, '2024-03-28'),
+-- ('11116537', 600.00,  2992.00, '2024-04-02'),
+-- ('11116737', 500.00,  2992.00, '2024-05-07'),
+-- ('11116804', 300.00,  2992.00, '2024-01-20'),
+-- ('11117318', 200.00,  2992.00, '2024-02-25'),
+-- ('11117536', 400.00,  2992.00, '2024-03-30'),
+-- ('11123762', 600.00,  2992.00, '2024-04-05'),
+-- ('11139245', 2992.00, 2992.00,  '2024-05-10'),
+-- ('11139828', 300.00,  2992.00, '2024-01-22'),
+-- ('11164744', 200.00, 2992.00,  '2024-02-27'),
+-- ('11170350', 400.00,  2992.00, '2024-03-05'),
+-- ('11172376', 600.00,  2992.00, '2024-04-10'),
+-- ('11208328', 2992.00,  2992.00, '2024-05-15'),
+-- ('11209640', 300.00,  2992.00, '2024-01-25'),
+-- ('11213307', 200.00,  2992.00, '2024-02-28'),
+-- ('11218951', 400.00,  2992.00, '2024-03-10'),
+-- ('11238291', 600.00, 2992.00,  '2024-04-15'),
+-- ('11246366', 500.00,  2992.00, '2024-05-20'),
+-- ('11252855', 2992.00,  2992.00, '2024-01-28'),
+-- ('11252857', 200.00,  2992.00, '2024-02-28'),
+-- ('11253931', 400.00, 2992.00,  '2024-03-15'),
+-- ('11254079', 600.00,  2992.00, '2024-04-20'),
+-- ('11254301', 500.00, 2992.00,'2024-04-20'),
+-- ('11254405', 300.00,  2992.00, '2024-01-30'),
+-- ('11262592', 200.00,  2992.00, '2024-03-02'),
+-- ('11264010', 400.00,  2992.00, '2024-03-17'),
+-- ('11275876', 600.00,  2992.00, '2024-04-22'),
+-- ('11285635', 500.00,  2992.00, '2024-05-27'),
+-- ('11292620', 300.00,  2992.00, '2024-02-02'),
+-- ('11293871', 200.00,  2992.00, '2024-03-04'),
+-- ('11296641', 2992.00,  2992.00, '2024-03-19'),
+-- ('11296662', 600.00,  2992.00, '2024-04-24'),
+-- ('11297849', 500.00,  2992.00, '2024-05-29'),
+-- ('11305528', 300.00,  2992.00, '2024-02-04'),
+-- ('11330446', 200.00,  2992.00, '2024-03-06'),
+-- ('11332163', 400.00,  2992.00, '2024-03-21'),
+-- ('11333321', 600.00,  2992.00, '2024-04-26'),
+-- ('11334401', 500.00,  2992.00, '2024-05-31'),
+-- ('11338323', 300.00,  2992.00, '2024-02-06'),
+-- ('11347946', 200.00,  2992.00, '2024-03-08'),
+-- ('11348310', 400.00,  2992.00, '2024-03-23'),
+-- ('11353826', 600.00,  2992.00, '2024-04-28'),
+-- ('11356825', 750.00,  2992.00, '2024-03-20'),
+-- ('11356825', 2992.00,  2992.00, '2024-03-20');
+
+-- SELECT student_number, amount_paid, total_fee, status
+-- FROM cped.fees_payments;
 
 -- CREATE TRIGGER trigger_set_payment_status
 -- BEFORE INSERT OR UPDATE ON cped.fees_payments
@@ -400,5 +540,8 @@
 
 -- SELECT cped.get_outstanding_fees();
 
-SELECT * FROM cped.fees_payments
+
+
+
+
 
